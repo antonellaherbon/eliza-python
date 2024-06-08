bc62f95a55ed6f3a8cb5f22dc50044d855f1f03a
Antonella <antoh2003@gmail.com>
2024-06-08 17:41:15 -0300

    update README.md

--- Eliza.py
+++ Eliza.py
@@ -1,36 +1,35 @@
 import random
 
-def Nombrearchivo(i):
-    for j in range(len(i)):
-        if i[j] in disparadoresTristeza:
-            return "PreguntasTristeza.txt"
-        elif i[j] in disparadoresMiedos:
-            return "PreguntasMiedos.txt"
-        elif i[j] in disparadoresFelicidad:
-            return "PreguntasFelicidad.txt"
-        else:
-            print("No puedo ayudarte con tu problema! Lo siento")
-    return ''
+def Nombrearchivo(lista_palabras,index):
+    if index < len(lista_palabras):
+        for clave,lista_disparadores in disparadores.items():
+            if lista_palabras[index] in lista_disparadores:
+                return 'Preguntas'+clave+'.txt'
+        return Nombrearchivo(lista_palabras,index+1)
+    return ""
+
+
+disparadores = {
+    'Tristeza': ['mal', 'llorar', 'triste', 'tristeza', 'soledad', 'desahuciado', 'solo', 'pena', 'duelo', 'depresivo', 'depresion', 'desanimado', 'sin'],
+    'Miedos': ['miedo', 'asustado', 'terror', 'fobia', 'susto', 'perplejo', 'angustia', 'atonito','ansioso'],
+    'Felicidad': ['feliz', 'contento', 'alegre', 'entusiasmado', 'bien', 'jocoso', 'alegria', 'placentero', 'jubilo']
+}
 
 print("Bienvenidos a ELIZA, el juego de ayuda psicológica")
 print("Para salir del juego, escriba 'Chau'")
 print("Hola, ¿Cómo te encuentras hoy?")
-
-disparadoresTristeza = ['mal','llorar','triste','tristeza','soledad','desahuciado','solo']
-disparadoresMiedos = ['miedo','asustado','terror','fobia','susto']
-disparadoresFelicidad = ['feliz','contento','alegre','entusiasmado']
-
 respuesta = input("Ingrese su respuesta: ")
-i = (respuesta.lower()).split()
-archivo = Nombrearchivo(i)
+
+archivo = Nombrearchivo(respuesta.lower().split(),0)
 listaPreguntas = []
 
-if archivo != '':
+if archivo == '':
+    print("No puedo ayudarte con tu problema! Lo siento")
+else:
     try:
-        archivoAbierto = open(archivo,"rt",encoding="UTF-8")
-        for i in range(0,3):
-            preg = archivoAbierto.readline()
-            listaPreguntas.append(preg)
+        archivoAbierto = open(archivo, "rt", encoding="UTF-8")
+        for pregunta in archivoAbierto:
+            listaPreguntas.append(pregunta)
     except FileNotFoundError:
         print("el archivo no existe")
     except OSError:
@@ -40,7 +39,6 @@ if archivo != '':
             archivoAbierto.close()
         except OSError:
             print("no se puede cerrar el archivo")
-
 
     pregunta_anterior = None
     while respuesta != 'chau':
--- README.md
+++ README.md
@@ -1,3 +1,14 @@
 # Eliza 
 
 Juego de preguntas hecho con python
+
+Este trabajo aborda  
+
+-Funciones  
+-Bucles  
+-Listas  
+-Cadenas de caracteres  
+-Excepciones  
+-Archivos  
+-Recursividad  
+-Diccionarios.
